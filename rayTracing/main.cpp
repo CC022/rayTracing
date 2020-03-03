db9c762762548a3789aea44658520ee2f740ca0a
zzc <local@local.com>
2020-03-02 20:50:30 -0800

    add vec3

--- rayTracing/main.cpp
+++ rayTracing/main.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <fstream>
+#include "vec3.hpp"
 
 int main(int argc, const char * argv[]) {
     using namespace std;
@@ -19,17 +20,12 @@ int main(int argc, const char * argv[]) {
     imgFile << "P3\n" << nx << " " << ny << "\n255\n";
     for (int j = ny-1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
-            float r = float(i) / float(nx);
-            float g = float(j) / float(ny);
-            float b = 0.2;
-            int ir = int(255.99 * r);
-            int ig = int(255.99 * g);
-            int ib = int(255.99 * b);
+            vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+            int ir = int(255.99 * col[0]);
+            int ig = int(255.99 * col[1]);
+            int ib = int(255.99 * col[2]);
             imgFile << ir << " " << ig << " " << ib << "\n";
         }
-        
     }
-    
-    
     return 0;
 }
