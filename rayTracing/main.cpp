60c808edab2b06f61e006b8160a58b1427ab2f9a
zzc <local@local.com>
2020-03-03 21:49:28 -0800

    diffuse materials

--- rayTracing/main.cpp
+++ rayTracing/main.cpp
@@ -16,10 +16,19 @@
 #include "random.hpp"
 #include "camera.hpp"
 
+vec3 randomInUnitSphere() {
+    vec3 p;
+    do {
+        p = 2.0*vec3(randomDouble(), randomDouble(), randomDouble()) - vec3(1, 1, 1);
+    } while (p.squared_length() >= 1.0);
+    return p;
+}
+
 vec3 color(const ray& r, hittable *world) {
     hit_record rec;
-    if (world->hit(r, 0.0, MAXFLOAT, rec)) {
-        return 0.5*vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
+    if (world->hit(r, 0.001, MAXFLOAT, rec)) {
+        vec3 target = rec.p + rec.normal + randomInUnitSphere();
+        return 0.5 * color(ray(rec.p, target - rec.p), world);
     } else {
         vec3 unit_direction = unit_vector(r.direction());
         float t = 0.5*(unit_direction.y() + 1.0);
@@ -30,9 +39,9 @@ vec3 color(const ray& r, hittable *world) {
 int main(int argc, const char * argv[]) {
     using namespace std;
     ofstream imgFile("img.ppm");
-    int width = 2000;
-    int height = 1000;
-    int ns = 100;
+    int width = 1000;
+    int height = 500;
+    int samples = 50;
     imgFile << "P3\n" << width << " " << height << "\n255\n";
     vec3 lowerLeftCorner(-2.0,-1.0,-1.0);
     vec3 horizontal(4.0,0.0,0.0);
@@ -48,14 +57,14 @@ int main(int argc, const char * argv[]) {
     for (int j = height-1; j >= 0; j--) {
         for (int i = 0; i < width; i++) {
             vec3 col(0, 0, 0);
-            for (int s = 0; s < ns; s++) {
+            for (int s = 0; s < samples; s++) {
                 float u = float(i + randomDouble()) / float(width);
                 float v = float(j + randomDouble()) / float(height);
                 ray r = cam.getRay(u, v);
                 col += color(r, world);
             }
-            col /= float(ns);
-        
+            col /= float(samples);
+            col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
             int ir = int(255.99 * col[0]);
             int ig = int(255.99 * col[1]);
             int ib = int(255.99 * col[2]);
